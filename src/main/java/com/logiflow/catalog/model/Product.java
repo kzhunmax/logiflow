dbe2c6b210aa37b3d37c30ea382743194b18b460
Maksym Korshun <korshunmax82@gmail.com>
2025-12-21 15:14:20 +0100

    LF-3 Create Flexible Product Entity

---
+++ src/main/java/com/logiflow/catalog/model/Product.java
@@ -0,0 +1,22 @@
+package com.logiflow.catalog.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Document(collection = "products")
+@Getter
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String sku;
+    private BigDecimal price;
+
+    private Map<String, Object> attributes;
+}
