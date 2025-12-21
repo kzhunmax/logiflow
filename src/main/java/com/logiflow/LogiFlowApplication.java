a8cd7eb63238ac111675257aae3637e2960d08d4
Maksym Korshun <korshunmax82@gmail.com>
2025-12-21 12:29:41 +0100

    LF-1 Init Backend Project

---
+++ src/main/java/com/logiflow/LogiFlowApplication.java
@@ -0,0 +1,13 @@
+package com.logiflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiFlowApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LogiFlowApplication.class, args);
+	}
+
+}
