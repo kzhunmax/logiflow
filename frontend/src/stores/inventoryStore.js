d87b41a0da0e581d03ec13685458d54bea35b4e4
Maksym Korshun <korshunmax82@gmail.com>
2026-01-04 01:00:40 +0100

    LF-47 Upgrade Inventory Store

--- frontend/src/stores/inventoryStore.js
+++ frontend/src/stores/inventoryStore.js
@@ -6,6 +6,13 @@ export const useInventoryStore = defineStore('inventory', () => {
   const stock = ref(null)
   const loading = ref(false)
   const error = ref(null)
+  const pagination = ref({
+    currentPage: 1,
+    totalPages: 1,
+    totalItems: 0,
+    pageSize: 10
+  })
+  const inventoryList = ref([])
 
   async function fetchStock(sku) {
     loading.value = true
@@ -28,6 +35,7 @@ export const useInventoryStore = defineStore('inventory', () => {
     try {
       await api.post('/inventory/stock', payload)
       await fetchStock(payload.sku)
+      await fetchAllInventory(pagination.value.currentPage);
     } catch (err) {
       if (err.response?.status === 409) {
         error.value = 'Cannot remove more stock than available'
@@ -39,9 +47,28 @@ export const useInventoryStore = defineStore('inventory', () => {
     }
   }
 
+  async function fetchAllInventory(page = 1) {
+    loading.value = true
+    error.value = null
+
+    try {
+      const response = await api.get('/inventory', {
+        params: {page: page - 1, size: pagination.value.pageSize}
+      })
+      inventoryList.value = response.data.content
+      pagination.value.totalPages = response.data.page.totalPages
+      pagination.value.totalItems = response.data.page.totalElements
+      pagination.value.currentPage = page
+    } catch (err) {
+      error.value = err.response?.data?.message || 'Failed to load inventories.'
+    } finally {
+      loading.value = false
+    }
+  }
+
   function setError(message) {
     error.value = message
   }
 
-  return {stock, loading, error, fetchStock, adjustStock, setError}
+  return {stock, loading, error, fetchStock, fetchAllInventory, adjustStock, setError}
 })
